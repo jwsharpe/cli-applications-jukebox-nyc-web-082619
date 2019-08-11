963a5bc32e17cc2311bbe38c2e88727ef1ef2878
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-11 00:56:16 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -3,7 +3,7 @@ def enter_command
   gets.strip
 end
 
-def help_command
+def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
