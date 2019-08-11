eeed8594ee81e7c5081d36e60c16a28f121be208
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-11 00:52:16 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -7,7 +7,8 @@ def help_command
   puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
-  puts "
+  puts "- play : lets you choose a song to play"
+  puts ""
 
  
 puts "Enter your name:"
